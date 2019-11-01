0fa13b4bf1297bb74b90d336e45499ec3f56a936
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-01 14:49:44 +0100

    corrected typehints for classad

--- classad/_classad.py
+++ classad/_classad.py
@@ -1,8 +1,10 @@
 from collections import OrderedDict
 from collections.abc import MutableMapping
-from typing import Iterator, _T_co, _KT, _VT_co, _VT, Any
+from typing import Iterator, Any, TYPE_CHECKING
 
-from classad._primitives import Undefined
+if TYPE_CHECKING:
+    from classad._expression import Expression
+    from classad._primitives import Undefined
 
 
 class ClassAd(MutableMapping):
@@ -11,32 +13,29 @@ class ClassAd(MutableMapping):
     def __init__(self):
         self._data = OrderedDict()
 
-    def __setitem__(self, key: _KT, value: _VT) -> None:
+    def __setitem__(self, key: str, value: "Expression") -> None:
         """
         Keynames that are reserved and, therefore, cannot be used: error, false, is,
             isnt, parent, true, undefined
-        :param key:
-        :param value:
-        :return:
         """
         key = key.casefold()
         if key in ["error", "false", "is", "isnt", "parent", "true", "undefined"]:
             raise ValueError(f'{key} is a reserved name')
         self._data[key] = value
 
-    def __delitem__(self, key: _KT) -> None:
+    def __delitem__(self, key: str) -> None:
         self._data.pop(key, None)
 
-    def __getitem__(self, key: _KT) -> _VT_co:
+    def __getitem__(self, key: str) -> "Expression":
         return self._data.get(key, Undefined())
 
     def __len__(self) -> int:
         return len(self._data)
 
-    def __iter__(self) -> Iterator[_T_co]:
+    def __iter__(self) -> Iterator[str]:
         return iter(self._data)
 
-    def evaluate(self, key: _KT, target: "ClassAd") -> Any:
+    def evaluate(self, key: str, target: "ClassAd") -> Any:
         """
         Perform a matchmaking between an expression defined by the named attribute
         key in the context of the target ClassAd.
