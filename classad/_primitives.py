7ae3666081f54d456e2116476f9449234aa37ff1
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-01 14:51:47 +0100

    added docstring for primitive data types

--- classad/_primitives.py
+++ classad/_primitives.py
@@ -1,6 +1,22 @@
+"""
+Literal constants: integer, float, string, boolean, error, or undefined
+"""
+
+
 class Undefined:
+    """
+    The keyword ``UNDEFINED`` (case insensitive) represents the ``UNDEFINED`` value.
+    """
     pass
 
 
 class Error:
+    """
+    The keyword ``ERROR`` (case insensitive) represents the ``ERROR`` value.
+    """
+    pass
+
+
+class Attribute:
+    """An attribute is the tuple from attribute name and expression."""
     pass
