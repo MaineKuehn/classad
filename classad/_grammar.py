28d259f0d418e748ebc57c6caa8e38fe6fc3a824
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-21 17:34:45 +0100

    introduced matchfirst to grammar parsing

--- classad/_grammar.py
+++ classad/_grammar.py
@@ -52,41 +52,53 @@ super_literal = pp.CaselessKeyword("super").setParseAction(
 octal_digit = pp.srange("[0-7]")
 nonzero_digit = pp.srange("[1-9]")
 decimal_digit = pp.nums
-reserved_word = (
-    error_literal
-    | boolean_literal
-    | pp.CaselessKeyword("isnt")
-    | pp.CaselessKeyword("is")
-    | parent_literal
-    | undefined_literal
-    | super_literal
+reserved_word = pp.MatchFirst(
+    (
+        error_literal,
+        boolean_literal,
+        pp.CaselessKeyword("isnt"),
+        pp.CaselessKeyword("is"),
+        parent_literal,
+        undefined_literal,
+        super_literal,
+    )
 ).setName("reserved_word")
-integer_literal = (
-    "0"
-    | pp.Word(nonzero_digit, decimal_digit)
-    | pp.Combine("0" + pp.Word(octal_digit))
-    | pp.Combine("0" + pp.Word(pp.srange("[xX]"), pp.hexnums))
+integer_literal = pp.MatchFirst(
+    (
+        "0",
+        pp.Word(nonzero_digit, decimal_digit),
+        pp.Combine("0" + pp.Word(octal_digit)),
+        pp.Combine("0" + pp.Word(pp.srange("[xX]"), pp.hexnums)),
+    )
 ).setParseAction(lambda s, l, t: HTCInt(t[0]))("integer*")
 exponent = pp.Combine(
     pp.CaselessLiteral("e") + pp.Optional(pp.oneOf("+ -")) + pp.Word(pp.nums)
 )
 floating_point_literal = (
-    (
-        pp.Combine(pp.Word(pp.nums) + "." + pp.Word(pp.nums) + pp.Optional(exponent))
-        | pp.Combine("." + pp.Word(pp.nums) + pp.Optional(exponent))
-        | pp.Combine(pp.Word(pp.nums) + exponent)
+    pp.MatchFirst(
+        (
+            pp.Combine(
+                pp.Word(pp.nums) + "." + pp.Word(pp.nums) + pp.Optional(exponent)
+            ),
+            pp.Combine("." + pp.Word(pp.nums) + pp.Optional(exponent)),
+            pp.Combine(pp.Word(pp.nums) + exponent),
+        )
     )
     .setParseAction(lambda s, l, t: HTCFloat(t[0]))
     .setResultsName("float*")
 )
 escaped_char = pp.Word("NnTtBbRrFf\\\"'", max=1)
-non_quote = (
-    pp.Combine("\\" + escaped_char)
-    | pp.Combine("\\" + pp.Word(octal_digit, min=1, max=2))
-    | pp.Combine(
-        "\\" + pp.Word(pp.srange("[0-3]"), max=1) + pp.Word(octal_digit, min=2, max=2)
+non_quote = pp.MatchFirst(
+    (
+        pp.Combine("\\" + escaped_char),
+        pp.Combine("\\" + pp.Word(octal_digit, min=1, max=2)),
+        pp.Combine(
+            "\\"
+            + pp.Word(pp.srange("[0-3]"), max=1)
+            + pp.Word(octal_digit, min=2, max=2)
+        ),
+        pp.Word(pp.printables, " ", excludeChars="\"'\\\n\r0"),
     )
-    | pp.Word(pp.printables, " ", excludeChars="\"'\\\n\r0")
 )
 unquoted_name = pp.Word(pp.alphas + "_", pp.alphanums + "_")
 quoted_name = pp.Combine((SQUOTE + pp.OneOrMore(non_quote | '"') + SQUOTE)).setName(
@@ -97,7 +109,9 @@ string_literal = (
     .setParseAction(lambda s, l, t: HTCStr(t[0]))
     .setName("string_literal")
 )
-literal = (floating_point_literal | integer_literal | string_literal).setName("literal")
+literal = pp.MatchFirst(
+    (floating_point_literal, integer_literal, string_literal)
+).setName("literal")
 attribute_name = (
     (unquoted_name | quoted_name)("attribute_name*")
     .setParseAction(lambda s, l, t: AttributeExpression.from_grammar(t[0]))
@@ -118,16 +132,18 @@ attribute_definition = (pp.Group(attribute_name + pp.Suppress("=") + expression)
     "attribute*"
 ).setName("attribute_definition")
 record_expression = (
-    (
-        LBRACKET
-        + pp.Group(
-            pp.Optional(
-                pp.delimitedList(attribute_definition, delim=";")
-                + pp.Optional(pp.Suppress(";"))
-            )
-        )("record*")
-        + RBRACKET
-        | pp.Group(pp.delimitedList(attribute_definition, delim=pp.Empty()))
+    pp.MatchFirst(
+        (
+            LBRACKET
+            + pp.Group(
+                pp.Optional(
+                    pp.delimitedList(attribute_definition, delim=";")
+                    + pp.Optional(pp.Suppress(";"))
+                )
+            )("record*")
+            + RBRACKET,
+            pp.Group(pp.delimitedList(attribute_definition, delim=pp.Empty())),
+        )
     )
     .setParseAction(lambda s, l, t: ClassAd.from_grammar(t[0]))
     .setName("record_expression")
@@ -143,49 +159,55 @@ function_call = (
     .setParseAction(lambda s, l, t: FunctionExpression.from_grammar(t))
     .setName("function_call")
 )
-atom = (
-    list_expression
-    | boolean_literal
-    | error_literal
-    | undefined_literal
-    | parent_literal
-    | super_literal
-    | function_call
-    | record_expression
-    | literal
-    | attribute_name
-    | LPAR + expression + RPAR
-).setName("atom")
-subscriptable = (  # introduced to remove recursion in suffix_expression
-    list_expression
-    | error_literal
-    | undefined_literal
-    | parent_literal
-    | super_literal
-    | target_literal
-    | function_call
-    | attribute_name
-    | record_expression
-    | LPAR + expression + RPAR
-).setName("subscriptable")
-suffix_expression << (
-    pp.Group(
-        "."
-        + pp.delimitedList(attribute_name, ".").setParseAction(
-            lambda s, l, t: AttributeExpression.from_grammar(t)
-        )
-    ).setParseAction(lambda s, l, t: AttributeExpression.from_grammar(t[0]))
-    | pp.Group(
-        subscriptable
-        + pp.Suppress(".")
-        + pp.delimitedList(attribute_name, ".").setParseAction(
-            lambda s, l, t: AttributeExpression.from_grammar(t)
-        )
-    ).setParseAction(lambda s, l, t: AttributeExpression.from_grammar(t[0]))
-    | pp.Group(subscriptable + LBRACKET + expression + RBRACKET).setParseAction(
-        lambda s, l, t: SubscriptableExpression.from_grammar(t[0])
+atom = pp.MatchFirst(
+    (
+        list_expression,
+        boolean_literal,
+        error_literal,
+        undefined_literal,
+        parent_literal,
+        super_literal,
+        function_call,
+        record_expression,
+        literal,
+        attribute_name,
+        LPAR + expression + RPAR,
     )
-    | atom
+).setName("atom")
+subscriptable = pp.MatchFirst(
+    (  # introduced to remove recursion in suffix_expression
+        list_expression,
+        error_literal,
+        undefined_literal,
+        parent_literal,
+        super_literal,
+        target_literal,
+        function_call,
+        attribute_name,
+        record_expression,
+        LPAR + expression + RPAR,
+    )
+).setName("subscriptable")
+suffix_expression << pp.MatchFirst(
+    (
+        pp.Group(
+            "."
+            + pp.delimitedList(attribute_name, ".").setParseAction(
+                lambda s, l, t: AttributeExpression.from_grammar(t)
+            )
+        ).setParseAction(lambda s, l, t: AttributeExpression.from_grammar(t[0])),
+        pp.Group(
+            subscriptable
+            + pp.Suppress(".")
+            + pp.delimitedList(attribute_name, ".").setParseAction(
+                lambda s, l, t: AttributeExpression.from_grammar(t)
+            )
+        ).setParseAction(lambda s, l, t: AttributeExpression.from_grammar(t[0])),
+        pp.Group(subscriptable + LBRACKET + expression + RBRACKET).setParseAction(
+            lambda s, l, t: SubscriptableExpression.from_grammar(t[0])
+        ),
+        atom,
+    )
 ).setName("suffix_expression")
 
 
@@ -213,14 +235,18 @@ arithmetic_expression << pp.infixNotation(
 )
 
 expression << pp.Group(
-    (
-        pp.Group(arithmetic_expression)("if")
-        + pp.Suppress("?")
-        + pp.Optional(expression, default=None)("then")
-        + pp.Suppress(":")
-        + pp.Group(expression)("else")
-    ).setParseAction(lambda s, l, t: TernaryExpression.from_grammar(t))
-    | arithmetic_expression
+    pp.MatchFirst(
+        (
+            (
+                pp.Group(arithmetic_expression)("if")
+                + pp.Suppress("?")
+                + pp.Optional(expression, default=None)("then")
+                + pp.Suppress(":")
+                + pp.Group(expression)("else")
+            ).setParseAction(lambda s, l, t: TernaryExpression.from_grammar(t)),
+            arithmetic_expression,
+        )
+    )
 ).setParseAction(lambda s, l, t: Expression.from_grammar(t[0])).setName("expression")
 
 
