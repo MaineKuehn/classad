be99e09a7b6ba429f84b500c2dfdb71645977b54
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-12-01 22:18:17 +0100

    added unit tests for correct lookup defaulting to target if not found in local classad

--- classad_tests/test_matchmaking.py
+++ classad_tests/test_matchmaking.py
@@ -1,5 +1,5 @@
 from classad import parse
-from classad._primitives import HTCInt
+from classad._primitives import HTCInt, Undefined
 
 
 def test_simple():
@@ -17,6 +17,32 @@ def test_simple():
 
 def test_nested():
     my_classad = parse("""rank = TARGET.a.b + TARGET.c""")
+    target_classad = parse("""[c = 1; a = [b = 2; d = 1]]""")
+    assert my_classad.evaluate(
+        key="rank", my=my_classad, target=target_classad
+    ) == HTCInt(3)
+
+
+def test_undefined():
+    assert parse("""rank = TARGET.a""").evaluate("rank") == Undefined()
+    assert parse("""rank = my.a""").evaluate("rank") == Undefined()
+
+
+def test_lookup():
+    my_classad = parse(
+        """
+    a = 10
+    b = c
+    """
+    )
+    target_classad = parse("""c = 1""")
+    assert my_classad.evaluate(key="b", my=my_classad, target=target_classad) == HTCInt(
+        1
+    )
+
+
+def test_nested_lookup():
+    my_classad = parse("""rank = a.b + c""")
     target_classad = parse("""[c = 1; a = [b = 2; d = 1]]""")
     assert my_classad.evaluate(
         key="rank", my=my_classad, target=target_classad
