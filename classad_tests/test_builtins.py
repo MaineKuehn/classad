ee026288f72ad60cbadfbb7830b6bff9bea7766f
Max Fischer <maxfischer2781@gmail.com>
2019-12-01 17:07:32 +0100

    added tests for RHS operations with builtins

---
+++ classad_tests/test_builtins.py
@@ -0,0 +1,62 @@
+import operator
+
+import pytest
+
+from classad._primitives import (
+    Error,
+    Undefined,
+    HTCInt,
+    HTCFloat,
+    # HTCList,
+    # HTCStr,
+    HTCBool,
+)
+from classad._operator import eq_operator, ne_operator
+
+
+NUMERIC_OPERATORS = (
+    operator.__add__,
+    operator.__sub__,
+    operator.__mul__,
+    operator.__truediv__,
+)
+COMPARISON_OPERATORS = (
+    operator.__eq__,
+    operator.__ne__,
+    operator.__gt__,
+    operator.__ge__,
+    operator.__le__,
+    operator.__lt__,
+    eq_operator,
+    ne_operator,
+)
+
+
+@pytest.fixture(params=[HTCInt, HTCFloat])
+def num_type(request):
+    return request.param
+
+
+def test_numbers(num_type):
+    def assert_similar(op, htc_value, htc_type, builtin_value):
+        """An HTC and builtin type behave similar with respect to an HTC value"""
+        assert op(htc_value, htc_type(builtin_value)) == op(
+            htc_value, htc_type(builtin_value)
+        )
+        assert op(htc_value, htc_type(builtin_value)) == op(htc_value, builtin_value)
+
+    for value in (2, 3, -3, 256, -127, 2 ** 8, 1e6) + (
+            () if num_type is HTCInt else (3-5, -3.5, 127.3, -123.2, 1e-8)
+    ):
+        for operation in NUMERIC_OPERATORS + COMPARISON_OPERATORS:
+            assert_similar(operation, HTCInt(1), num_type, value)
+            assert_similar(operation, HTCInt(123), num_type, value)
+            assert_similar(operation, HTCInt(-123), num_type, value)
+            assert_similar(operation, HTCFloat(1), num_type, value)
+            assert_similar(operation, HTCFloat(123), num_type, value)
+            assert_similar(operation, HTCFloat(-123), num_type, value)
+        for operation in COMPARISON_OPERATORS:
+            assert_similar(operation, HTCBool(True), num_type, value)
+            assert_similar(operation, HTCBool(False), num_type, value)
+            assert_similar(operation, Error(), num_type, value)
+            assert_similar(operation, Undefined(), num_type, value)
