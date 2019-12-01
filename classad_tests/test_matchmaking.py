88cc4ea3bcc6b4b52dc118c61a8b659346361742
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-12-01 16:41:15 +0100

    added matchmaking test with nested attributes

--- classad_tests/test_matchmaking.py
+++ classad_tests/test_matchmaking.py
@@ -13,3 +13,11 @@ def test_simple():
     assert my_classad.evaluate(
         key="rank", my=my_classad, target=target_classad
     ) == HTCInt(18)
+
+
+def test_nested():
+    my_classad = parse("""rank = TARGET.a.b + TARGET.c""")
+    target_classad = parse("""[c = 1; a = [b = 2; d = 1]]""")
+    assert my_classad.evaluate(
+        key="rank", my=my_classad, target=target_classad
+    ) == HTCInt(3)
